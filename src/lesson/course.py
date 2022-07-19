f8ea68eccd38122f2774c476fe3bbb6e6e2fdb4d
brookCITS <brook@codeintheschools.org>
2022-07-19 11:22:18 -0400

    added more comments

--- src/lesson/course.py
+++ src/lesson/course.py
@@ -1,12 +1,16 @@
 #define the class
 class Course:
 
-    
+    #define the class variables
+
+    #define the init method
     def __init__(self):
         self.id = ''
         self.name = ''
         self.year = ''
         self.semester = ''
         self.professor = ''
+
+    #define other methods
 
 #use the class
