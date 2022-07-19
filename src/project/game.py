a7fe24fe685caec56fe6947234844f7126e87ed9
brookCITS <brook@codeintheschools.org>
2022-07-19 10:39:17 -0400

    answered question 3

--- src/project/game.py
+++ src/project/game.py
@@ -5,6 +5,12 @@ player ={
 }
 
 #Your code here
+def move(speed):
+    player['position'] = player['position'] + speed
+
+    return player['position']
+
+
 
 
 
@@ -15,8 +21,10 @@ run_code = True
 while run_code:
     user_input = int(input("What do you want to do?\nEnter 1 to move left\n2 to move right\n3 to exit"))
     #Your code here
+    if user_input == 1:
+        user_input = int(input("How fast to the left?"))
+        print("Updated Position:"+ str(move(user_input)))
 
-
-
-
-    
+    elif user_input == 3:
+            print("Good Bye!")
+            run_code = False
