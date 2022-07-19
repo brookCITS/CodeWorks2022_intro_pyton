056af1c9302d4bc7eda024cc7c3a0f1b1bb2b39a
brookCITS <brook@codeintheschools.org>
2022-07-19 11:45:24 -0400

    more comments

--- src/project/game.py
+++ src/project/game.py
@@ -5,16 +5,16 @@ class Character:
 
     #define the the init function
     def __init__(name):
-
+        #your code here
 
     #add the methods
     def move():
-
+        #your code here
     def level_up():
-
+        #your code here
     def kill():
-
+        #your code here
     def bonus():
-
+        #your code here
 
 #create 4 instance of this class and test all the methods twice
