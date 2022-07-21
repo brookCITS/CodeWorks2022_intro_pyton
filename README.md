d914ccb4fde9225749e537cf40cba39d5b9aed1b
brookCITS <brook@codeintheschools.org>
2022-07-21 12:02:51 -0400

    added more examples

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Word Count
 Write a program that
-- reads text from a file called paragraph.txt
+- reads text from a file called "paragraph.txt"
 - turns the text into a dictionary of word counts (use code form proj7)
-- saves it into another file called wordcount.txt
+- saves it into another file called "wordcount.txt"
+
+## line count
+Write a function in python to count the number of lines from a text file "story.txt". Call the function to see if it works
+- update the function so that it can take the file name to be opened as an input
--- file_example.py
+++ file_example.py
@@ -1,4 +1,37 @@
-fp = open(“files/loremIpsum.txt”, “x”)
-for i in range(5):
-    fp.write(“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n”)
-fp.close()
+#read from python
+#1
+ipsum_file = open(“files/ipsum.txt”)
+for line in ipsum_file:
+    print(line)
+    #print(line.rstrip())
+
+#2
+#ipsum_file.seek(0)
+#lines = ipsum_file.readlines()
+#print(lines)
+
+#3
+#with open("files/ipsum.txt") as ipsum_file:
+#    line = ipsum_file.readlines()
+#    print(line)
+
+
+#write to python
+#1
+#with open('files/write.txt', 'w') as file:
+#    file.write("I love Python so much\n")
+#    file.write("I really love Python soooooooo much")
+
+
+#with open('files/write.txt', 'w') as file:
+#    file.write("\nI dream about python sometimes!")
+
+
+#2
+#quotes = [
+#    '\npeace',
+#    '\nlove',
+#    '\nand rock n roll'
+#]
+#with open('files/write.txt', 'a') as file:
+#    file.write("\nI dream about python sometimes!")
