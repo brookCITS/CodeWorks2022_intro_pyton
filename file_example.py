5fc7b0058eea4fb5f96bc959fb755be204742c26
brookCITS <brook@codeintheschools.org>
2022-07-21 12:40:10 -0400

    Went over lesson and updated lesson files

--- file_example.py
+++ file_example.py
@@ -1,14 +1,15 @@
 #read from python
 #1
-ipsum_file = open(“files/ipsum.txt”)
-for line in ipsum_file:
-    print(line)
-    #print(line.rstrip())
-
+#ipsum_file = open("files/ipsum.txt")
+#for line in ipsum_file:
+    #print(line)
+#    print(line.rstrip())
+#ipsum_file.close()
 #2
-#ipsum_file.seek(0)
+#ipsum_file.seek(44)
 #lines = ipsum_file.readlines()
 #print(lines)
+
 
 #3
 #with open("files/ipsum.txt") as ipsum_file:
@@ -16,22 +17,32 @@ for line in ipsum_file:
 #    print(line)
 
 
+
+
 #write to python
 #1
-#with open('files/write.txt', 'w') as file:
-#    file.write("I love Python so much\n")
-#    file.write("I really love Python soooooooo much")
+with open('files/write.txt', 'w') as file:
+    file.write("I\nlove\nPython\nso\nmuch\n")
+    file.write("I really love Python soooooooo much")
 
 
-#with open('files/write.txt', 'w') as file:
-#    file.write("\nI dream about python sometimes!")
+with open('files/write.txt', 'a') as file:
+    file.write("\nI dream about python sometimes!")
 
 
 #2
-#quotes = [
-#    '\npeace',
-#    '\nlove',
-#    '\nand rock n roll'
-#]
-#with open('files/write.txt', 'a') as file:
-#    file.write("\nI dream about python sometimes!")
+quotes = [
+    '\npeace',
+    '\nlove',
+    '\nand rock n roll'
+]
+with open('files/write.txt', 'a') as file:
+    file.writelines(quotes)
+
+try:
+    file = open('files/myfile.txt', 'x')
+    file.write(str({'1':'one', '2':'two'}))
+    file.close()
+
+except:
+    print("Unable to append to file")
