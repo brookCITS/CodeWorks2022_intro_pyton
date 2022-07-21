5a5971a5fdffb71b46a4bec435b24f18fafaf414
brookCITS <brook@codeintheschools.org>
2022-07-21 09:02:21 -0400

    started editing the project

---
+++ wordCount.py
@@ -0,0 +1,20 @@
+
+
+par =
+par = par.replace(',','')
+par = par.replace('\'','')
+
+#turn the paragraph into a list of words
+list = par.split(' ')
+
+#the variable where you will store the dictionary file
+myDict = dict()
+
+for word in list:
+    if word not in myDict:
+        myDict.update({ word : 1 })
+    else:
+        myDict[word] = myDict[word] + 1
+
+
+print(myDict)
